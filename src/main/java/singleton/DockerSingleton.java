ea765ba4daa021cf7c088e747bb389be2bcaf9bd
apreeti-tw <apreeti@thoughtworks.com>
2021-06-13 23:35:42 +0530

    Convert DockerInitialiser to singleton class

---
+++ src/main/java/singleton/DockerSingleton.java
@@ -0,0 +1,47 @@
+package singleton;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class DockerSingleton {
+    public static DockerSingleton seleniumGrid;
+
+    public static DockerSingleton getSeleniumGrid(){
+        if (seleniumGrid == null){
+            seleniumGrid = new DockerSingleton();
+            return seleniumGrid;
+        }
+        return seleniumGrid;
+    }
+
+    public void startStopDocker(String op, String message) throws IOException, InterruptedException {
+        if(op.equals("start"))
+            Runtime.getRuntime().exec(System.getProperty("user.dir")+"/startDocker.sh");
+        else
+            Runtime.getRuntime().exec(System.getProperty("user.dir")+"/stopDocker.sh");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND, 30);
+        boolean hubStarted = false;
+        Thread.sleep(3000);
+
+        while (System.currentTimeMillis() < calendar.getTimeInMillis()){
+            if(hubStarted){
+                break;
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/output.txt"));
+            String line;
+            while((line = br.readLine())!=null){
+                if(line.contains(message)){
+                    System.out.println("Selenium Hub started/stopped successfully");
+                    hubStarted = true;
+                    br.close();
+                    break;
+                }
+            }
+        }
+    }
+}
--- src/test/java/docker/StartStopDocker.java
+++ src/test/java/docker/StartStopDocker.java
@@ -2,7 +2,7 @@ package docker;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import resources.DockerOperation;
+import singleton.DockerSingleton;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,13 +10,13 @@ import java.io.IOException;
 public class StartStopDocker {
     @BeforeSuite
     public void startDocker() throws IOException, InterruptedException {
-        DockerOperation.docker("start", "Started Selenium Hub");
+        DockerSingleton.getSeleniumGrid().startStopDocker("start", "Started Selenium Hub");
     }
 
     @AfterSuite
     public void stopDocker() throws IOException, InterruptedException {
         File file = new File("output.txt");
-        DockerOperation.docker("stop", "Removing selenium-hub");
+        DockerSingleton.getSeleniumGrid().startStopDocker("stop", "Removing selenium-hub");
         file.delete();
     }
 }
